301636433b28ea0fc9724b517f6b17a168d7d8a9
PKgothatso <ksp331@outlook.com>
2024-01-17 05:34:59 -0800

    Monty files

---
+++ monty_mod.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+/**
+ * f_mod - Computes the rest of the division of the 2nd top element
+ * of the stack by the top element of the stack.
+ * @head: A pointer to the head of the stack.
+ * @counter: The current line number.
+ * Return: No return.
+ */
+void f_mod(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
