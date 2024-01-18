d806a2989370ad4e364a17370400608da2f960d5
PKgothatso <ksp331@outlook.com>
2024-01-18 00:40:51 -0800

    Prints the top of the stack

---
+++ monty_stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - Prints the top of the stack.
+ * @head: A double pointer to the head of the stack.
+ * @counter: The line number
+ * Return: No return.
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
